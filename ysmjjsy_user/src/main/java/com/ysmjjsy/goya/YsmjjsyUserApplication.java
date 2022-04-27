2dc68c669f88a9498ddea12c2e4cf0ee1deb9ab6
goya <773949603@qq.com>
2022-04-27 23:07:15 +0800

    user init

---
+++ ysmjjsy_user/src/main/java/com/ysmjjsy/goya/YsmjjsyUserApplication.java
@@ -0,0 +1,13 @@
+package com.ysmjjsy.goya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsmjjsyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsmjjsyUserApplication.class, args);
+	}
+
+}
---
+++ ysmjjsy_user/src/test/java/com/ysmjjsy/goya/YsmjjsyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysmjjsy.goya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsmjjsyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
